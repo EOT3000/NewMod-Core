96d2a1c2a87fc88447735f5f55c5ffa34d5c028f
EOT3000 <43685885+EOT3000@users.noreply.github.com>
2024-02-20 14:12:00 -0500

    aaaa

---
+++ Villagers/src/main/java/me/fly/newmod/villagers/sddds/MineBehavior.java
@@ -0,0 +1,14 @@
+package me.fly.newmod.villagers.sddds;
+
+import net.minecraft.world.entity.ai.behavior.Behavior;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.ai.memory.MemoryStatus;
+import net.minecraft.world.entity.npc.EntityVillager;
+
+import java.util.Map;
+
+public class MineBehavior extends VillagerBehavior {
+    public MineBehavior(Map<MemoryModuleType<?>, MemoryStatus> requiredMemoryState) {
+        super(null);
+    }
+}
